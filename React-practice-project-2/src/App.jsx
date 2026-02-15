5159c90ea972983042a2def1df824aaba8ee4ce8
kmsmahi <salauddinmahi2002@gmail.com>
2026-02-15 12:33:28 +0600

    add the navbar and banner

--- React-practice-project-2/src/App.jsx
+++ React-practice-project-2/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import navImg from './assets/logo.png'
+import coinImg from './assets/coin.png'
+import bannerImg from './assets/banner-main.png'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    {/* navbar starting */}
+      
+      <div className="navbar flex  p-6 bg-gray-800 text-white max-w-[1200px] mx-auto">
+  <div className="flex-1">
+    <a className="text-xl">
+    <img className='w-[60px] h-[60px]'  src={navImg} alt="" /></a>
+  </div>
+  <div className="flex gap-2 items-center">
+    <span className='text-xl font-semibold'>600000000</span>
+    {/* <span className='text-xl font-semibold'>coins</span> */}
+    <img className='w-[20px] h-[20px]' src={coinImg} alt="" />
+  </div>
+</div>
+
+    {/* navbar ending */}
+
+     {/* banner starting */}
+
+     <section className='max-w-[1200px] mx-auto'>
+      <div className="banner flex items-center gap-10 mt-10">
+        <div className="banner-left flex-1">
+          <h1 className='text-4xl font-bold mb-4'>Welcome to the Coin World</h1>
+          <p className='text-lg text-gray-600 mb-6'>Discover the exciting world of cryptocurrencies and digital assets. Stay updated with the latest trends and insights in the crypto market.</p>
+          <button className='px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300'>Get Started</button>
+        </div>
+        <div className="banner-right flex-1">
+          <img src={bannerImg} alt="Banner Image" className='w-full h-auto' />
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
+      
+
+     </section>
+
     </>
   )
 }
